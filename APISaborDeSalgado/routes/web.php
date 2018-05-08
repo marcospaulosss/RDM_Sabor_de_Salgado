fca77274861e5568cebe8a709e4e26f60d12c8c8
marcos paulo de sousa sntos <marcospaulo@MacBook-Pro-de-marcos.local>
2018-05-08 15:02:15 -0300

    teste deploy automatic

--- APISaborDeSalgado/routes/web.php
+++ APISaborDeSalgado/routes/web.php
@@ -15,3 +15,8 @@ Route::get('/', function () {
     return view('welcome');
     //return view('spa');
 });
+
+Route::get('/login', function () {
+  return view('welcome');
+  //return view('spa');
+});
